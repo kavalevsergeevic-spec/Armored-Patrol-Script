6a3d8bd7c3e2c0fb0cd8890e95de71d6a3f46703
kavalevsergeevic-spec <kavalevsergeevic@gmail.com>
2026-03-10 12:40:30 +0500

    Create Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,20 @@
+local function sendNotification(title, text)
+    game:GetService("StarterGui"):SetCore("SendNotification", {
+        Title = title,
+        Text = text,
+        Duration = 3
+    })
+end
+
+if game.PlaceId == 10518166490 then
+    local success, err = pcall(function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/ТВОЙ_АККАУНТ/Armored-Patrol-Sript/main/CreatorHubX.lua"))()
+    end)
+    if success then
+        sendNotification("CreatorHubX", "Скрипт загружен!")
+    else
+        sendNotification("CreatorHubX", "Ошибка: " .. tostring(err))
+    end
+else
+    sendNotification("CreatorHubX", "❌ Запусти ARMORED PATROL!")
+end
